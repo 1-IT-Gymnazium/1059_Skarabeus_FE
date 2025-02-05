dfd3388b156498293dc1550421cbb05b7e900e73
kryštof Kadlčák <kkadlcak@gmail.com>
2025-02-05 10:57:10 +0100

    eeeeee

--- Skarabeus_Ui/src/app/app.routes.ts
+++ Skarabeus_Ui/src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { DefaultComponent } from './layouts/default/default.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { NotFoundPageComponent } from './pages/errors/not-found-page/not-found-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+      path: '',
+      component: DefaultComponent,
+      children: [
+        //{ path: '', component: HomePageComponent, title: 'Home', canActivate: [AuthGuard] },
+        { path: 'login', component: LoginPageComponent, title: 'Login' },
+        {
+          path: 'not-found',
+          component: NotFoundPageComponent,
+          title: 'Not Found ',
+        },
+        /*{
+          path: 'post/detail/:postId',
+          component: PostDetailPage,
+          title: 'Post Detail',
+          resolve: { post: postDetailResolver },
+        },*/
+      ],
+    },
+    { path: '**', redirectTo: '/not-found' },
+  ];
